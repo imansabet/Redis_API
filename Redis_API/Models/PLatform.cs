4065c6c2ca7f752b5da98a34acdbba31c75eb9ca
iman <dev.imansabet@gmail.com>
2024-02-11 11:15:32 +0330

    Create Model : Platform.cs

---
+++ Redis_API/Models/PLatform.cs
@@ -0,0 +1,9 @@
+﻿namespace Redis_API.Models
+{
+    public class Platform
+    {
+        public string Id { get; set; } = $"platform:{Guid.NewGuid().ToString()}";
+        public string Name { get; set; } = String.Empty; 
+
+    }
+}
