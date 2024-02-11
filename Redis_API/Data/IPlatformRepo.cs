897d607e2ca7a18f36bb19499b67c6da2bd1457a
iman <dev.imansabet@gmail.com>
2024-02-11 12:01:59 +0330

    Create Repository Interface : 'IPlatformRepo' , Implement Interface : 'RedisPlatformRepo'

---
+++ Redis_API/Data/IPlatformRepo.cs
@@ -0,0 +1,11 @@
+﻿using Redis_API.Models;
+
+namespace Redis_API.Data
+{
+    public interface IPlatformRepo
+    {
+        void CreatePlatform(Platform platform);
+        Platform? GetPlatformById(string id);
+        IEnumerable<Platform> GetAllPlatforms();
+    }
+}
